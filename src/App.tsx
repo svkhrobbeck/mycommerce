8b232693cff195c67b9de8562bdb9ed7bd175b6a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-01 20:38:30 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App: React.FC = (): JSX.Element => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {},
+      fontFamily: {},
+    },
+  },
+  screens: {
+    sm: "576px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+    "2xl": "1536px",
+  },
+  plugins: [],
+};
