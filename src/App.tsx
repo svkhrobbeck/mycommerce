49a9def808a73f21f4be005f3fed8d856f06341f
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 19:50:48 +0500

    Add loading to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { FC, useContext, useEffect } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import "./service/axios";
 import { Footer, Header } from "./components";
 import Router from "./router/Router";
 import AuthService from "./service/auth";
 import { Context } from "./context/Context";
+import { styles } from "./constants/styles";
 
 const App: FC = (): JSX.Element => {
   const { setAuth } = useContext(Context);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const getUser = async () => {
+    setIsLoading(true);
     try {
       const user = await AuthService.userGet();
       setAuth(prev => ({ ...prev, user }));
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -25,7 +30,7 @@ const App: FC = (): JSX.Element => {
     <>
       <Header />
       <main className="flex-grow-[1] flex flex-col">
-        <Router />
+        {isLoading ? <div className={`${styles.container} py-4 md:py-6`}>Loading...</div> : <Router />}
       </main>
       <Footer />
     </>
