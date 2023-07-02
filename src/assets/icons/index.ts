7fb767bf59978822f0177ba090b0af66e2930117
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 23:59:46 +0500

    Add star icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,3 +4,5 @@ export { default as iconFacebook } from "./icon-facebook.svg";
 export { default as iconTwitter } from "./icon-twitter.svg";
 export { default as iconInstagram } from "./icon-instagram.svg";
 export { default as spinner } from "./spinner.svg";
+export { default as iconStar } from "./star.svg";
+export { default as iconWhiteStar } from "./white-star.svg";
