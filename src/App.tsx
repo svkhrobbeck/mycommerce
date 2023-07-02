ef531a2515552be45d8a510f7d293d2af4e04b65
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 05:08:55 +0500

    Add layouts and getUser async function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import * as React from "react";
+import { FC, useContext, useEffect } from "react";
+import "./service/axios";
+import { Footer, Header } from "./components";
+import Router from "./router/Router";
+import AuthService from "./service/auth";
+import { Context } from "./context/Context";
 
-const App: React.FC = (): JSX.Element => {
-  return <div>App</div>;
+const App: FC = (): JSX.Element => {
+  const { setAuth } = useContext(Context);
+
+  const getUser = async () => {
+    try {
+      const user = await AuthService.userGet();
+      setAuth(prev => ({ ...prev, user }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main className="flex-grow-[1]">
+        <Router />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
