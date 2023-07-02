4dca10c8c4df5dc0f7ade9c12636624f08dd7b35
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 05:09:54 +0500

    Add icons exporter to assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as iconCart } from "./cart.svg";
+export { default as iconUser } from "./user.svg";
+export { default as iconFacebook } from "./icon-facebook.svg";
+export { default as iconTwitter } from "./icon-twitter.svg";
+export { default as iconInstagram } from "./icon-instagram.svg";
