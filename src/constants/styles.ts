cbaa5e56fc6e83cba99478455a1ed91364bf8f4d
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 21:20:07 +0500

    Add new buttons styles

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -12,7 +12,9 @@ export const styles: IStyles = {
   buttonPurpleOutlined:
     "inline-block rounded-md border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm text-white hover:bg-transparent hover:text-indigo-600 focus:ring transition-all duration-300",
   buttonPurple:
-    "inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring mx-auto",
-  buttonYellow: "block w-full mb-2 rounded bg-yellow-400 p-3 text-sm font-medium transition",
+    "inline-block px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring",
+  buttonYellow: "outline-none block w-full mb-2 rounded bg-yellow-400 p-3 text-sm font-medium transition",
   badgeYellow: "whitespace-nowrap mb-3 inline-block bg-yellow-400 px-[12px] py-[6px] text-xs font-medium",
+  buttonGreen: "block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700",
+  buttonLightGray: "rounded-md bg-gray-100 px-5 py-3 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 block",
 };
