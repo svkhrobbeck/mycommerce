169150d3233bf3404bd75afe366a24c38173e12d
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-03 00:01:02 +0500

    Change paragraph and buttons styles

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -7,13 +7,13 @@ export const styles: IStyles = {
   flexCenter: "flex justify-center items-center",
   container: "max-w-[1440px] w-full mx-auto px-[20px]",
   flexCol: "flex flex-col",
-  paragraph: "text-white text-[15px] font-regular leading-[1.5] md:font-medium",
+  paragraph: "text-white text-[13px] sm:text-[14px] md:text-[15px] font-regular leading-[1.3] md:font-medium",
   py: "py-4 md:py-6",
   buttonPurpleOutlined:
     "inline-block rounded-md border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm text-white hover:bg-transparent hover:text-indigo-600 focus:ring transition-all duration-300",
   buttonPurple:
     "inline-block px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring",
-  buttonYellow: "outline-none block w-full mb-2 rounded bg-yellow-400 p-3 text-sm font-medium transition",
+  buttonYellow: "outline-none block w-full rounded bg-yellow-400 hover:bg-yellow-500 p-3 text-sm font-medium transition",
   badgeYellow: "whitespace-nowrap mb-3 inline-block bg-yellow-400 px-[12px] py-[6px] text-xs font-medium",
   buttonGreen: "block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700",
   buttonLightGray: "rounded-md bg-gray-100 px-5 py-3 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 block",
