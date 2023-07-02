ab3eed490478b289cd88377234a53ec83893fe8d
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 12:17:09 +0500

    Create products service for calling to api

---
+++ src/service/products.ts
@@ -0,0 +1,19 @@
+import { IParams } from "../interfaces";
+import axios from "./axios";
+
+const ProductsService = {
+  async getProducts(params: IParams) {
+    const { data } = await axios.get("products", { params });
+    return data;
+  },
+  async getProduct(id: string | null) {
+    const data = await axios.get(`products/${id}`);
+    return data;
+  },
+  async getProductCategories() {
+    const { data } = await axios.get("categories");
+    return data;
+  },
+};
+
+export default ProductsService;
