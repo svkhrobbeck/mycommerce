7e304a3fd193210d66041d37cd9001773b82dc71
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 05:11:02 +0500

    Create styles for tailwind class-names

---
+++ src/constants/styles.ts
@@ -0,0 +1,10 @@
+import { IStyles } from "../interfaces";
+
+export const styles: IStyles = {
+  flexBetween: "flex justify-between items-center",
+  flexStart: "flex justify-start items-center",
+  flexEnd: "flex justify-end items-center",
+  flexCenter: "flex justify-center items-center",
+  container: "max-w-[1440px] w-full mx-auto px-[20px]",
+  paragraph: "text-white text-[15px] leading-[1.5] font-medium",
+};
