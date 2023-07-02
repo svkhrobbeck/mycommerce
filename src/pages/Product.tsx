83fe107d7b9a096841a28ffd0ccd277c37b31073
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 19:55:15 +0500

    Update project page component

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,75 +1,60 @@
 import { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ProductsService from "../service/products";
-import { IProduct } from "../interfaces";
+import { IParams, IProduct } from "../interfaces";
 import { styles } from "../constants/styles";
-import { SwiperSlide, Swiper } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import { ProductCard, ProductsList, SwiperImageSliders } from "../components";
+import { CART_LOCALSTORAGE } from "../constants/constants";
+import { getStorageParse } from "../helpers/localStorage";
+import setToCart from "../helpers/setToCart";
 
 const Product: FC = (): JSX.Element => {
+  const [keys, setKeys] = useState<number[]>(getStorageParse(CART_LOCALSTORAGE));
   const { id } = useParams();
   const [product, setProduct] = useState<IProduct | null>(null);
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [params, setParams] = useState<IParams>({});
 
-  console.log(product);
+  const getProduct = async (id: string) => {
+    const data: IProduct = await ProductsService.getProduct(id);
 
-  const getProduct = async (id: string | null) => {
-    const { data } = await ProductsService.getProduct(id);
+    setProduct(data);
+    setParams({ categoryId: data?.category?.id });
+  };
 
-    setProduct(data || null);
+  const getProducts = async () => {
+    const products = await ProductsService.getProducts(params);
+    setProducts(products);
   };
 
   useEffect(() => {
-    getProduct(id || null);
+    getProduct(id as string);
+    window.scrollTo(0, 0);
   }, [id]);
+
+  useEffect(() => {
+    getProducts();
+  }, [product]);
+
+  const addToCart = () => setKeys(setToCart(Number(id)));
 
   return (
     <div className={`${styles.container} py-4 md:py-6`}>
-      <div className="mb-3">
-        <h3 className="text-[36px] mb-1.5 font-bold uppercase text-gray-700 group-hover:underline group-hover:underline-offset-4">
-          {product?.title}
-        </h3>
-        <p className="mb-1.5 max-w-[390px] text-md text-gray-500">{product?.description}</p>
-        <p className="max-w-[390px] font-semibold text-xl text-gray-800">Price: ${product?.price}</p>
-      </div>
-      <div className="relative group block h-[550px] sm:h-[450px] mb-4">
-        <img src={product?.images[0]} className="absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0" />
-        <img src={product?.images[1]} className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100" />
-      </div>
-      <Swiper
-        spaceBetween={2}
-        slidesPerView={2}
-        loop={true}
-        navigation
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        breakpoints={{
-          "320": {
-            slidesPerView: 1,
-          },
-          "640": {
-            slidesPerView: 2,
-          },
-          "990": {
-            slidesPerView: 3,
-          },
-        }}
-      >
-        {product?.images.map(image => (
+      <div className="mb-6">
+        {!!product?.id && (
           <>
-            <SwiperSlide>
-              <img className="w-full select-none h-auto" src={image} key={image} />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img className="w-full select-none h-auto" src={image} key={image} />
-            </SwiperSlide>
+            <h3 className="text-[36px] mb-[6px] font-bold uppercase text-gray-700">{product?.title}</h3>
+            <p className="mb-1.5 max-w-[390px] text-md text-gray-500">{product?.description}</p>
+            <p className="max-w-[390px] mb-2 font-semibold text-xl text-gray-800">Price: ${product?.price}</p>
+            <SwiperImageSliders images={product.images} isOne={false} />
+            <button className={`${styles.buttonYellow} hover:scale-[1.02]`} onClick={addToCart}>
+              {keys.includes(Number(id)) ? "Added" : "Add to Cart"}
+            </button>
           </>
-        ))}
-      </Swiper>
+        )}
+      </div>
+      {!!products.length && <h3 className="text-2xl md:text-4xl lg:text-5xl uppercase mb-3 font-semibold text-center">Similar products</h3>}
+      <ProductsList products={products} />
     </div>
   );
 };
