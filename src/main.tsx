9bd883fff561638f873d96c43aab3c23b72307fc
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 05:07:27 +0500

    Add BrowserRouter and ContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,9 @@ import store from "./store/index.ts";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+  <ContextProvider>
+    <BrowserRouter>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </BrowserRouter>
+  </ContextProvider>
 );
