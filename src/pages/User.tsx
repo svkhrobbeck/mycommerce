502e37141593ce5b6ec743c018ddb64b002e184d
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 22:27:03 +0500

    Add updateUser function to user page component

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,14 +1,19 @@
-import { FC, useContext, useState } from "react";
+import { FC, useContext, useState, ChangeEvent } from "react";
 import { Context } from "../context/Context";
 import { useNavigate } from "react-router-dom";
 import { styles } from "../constants/styles";
 import { CustomInput } from "../components";
+import { IAxiosResponse, ICustomInput } from "../interfaces";
+import AuthService from "../service/auth";
+import errorToString from "../helpers/errorToString";
 const User: FC = (): JSX.Element => {
   const navigate = useNavigate();
   const { auth, setAuth } = useContext(Context);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [name, setName] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [err, setErr] = useState<string | null>(null);
 
   const inputs: ICustomInput[] = [
     { type: "text", placeholder: "New Name", styles: "", value: name, setValue: setName },
@@ -22,12 +27,33 @@ const User: FC = (): JSX.Element => {
     navigate("/login");
   };
 
+  const handleSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setErr(null);
+    try {
+      const data = { name, email, password };
+      const user = await AuthService.userUpdate(auth?.user?.id as number, data);
+      setAuth(prev => ({ ...prev, user }));
+      navigate("/");
+    } catch (err) {
+      const error = err as IAxiosResponse;
+      setErr(errorToString(error?.response?.data));
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={`${styles.container} ${styles.py}`}>
-      <form className="rounded-xl border border-gray-700 bg-gray-800 p-4">
+      <h2 className="text-4xl font-semibold text-center mb-4 uppercase">Update Profile</h2>
+      <form onSubmit={handleSubmit} className="rounded-xl border border-gray-700 bg-gray-800 p-4">
         <div className="mb-4 flex w-full items-center gap-4 flex-wrap">
           <img className="w-[80px] rounded-full" alt="Developer" src={auth?.user?.avatar} />
           <div>
+            <h3 className="text-xl font-medium text-white capitalize mb-1">Name: {auth?.user?.name}</h3>
+            <p className="leading-none text-md font-medium text-gray-300 mb-2">Email: {auth?.user?.email}</p>
+            <p className="leading-none text-md font-medium text-gray-300">Password: {auth?.user?.password}</p>
           </div>
           <button className={`${styles.buttonGreen} ml-auto self-center flex-grow-[1] xs:flex-grow-0`} onClick={handleLogoutClick}>
             Logout
@@ -38,9 +64,14 @@ const User: FC = (): JSX.Element => {
             <CustomInput key={input.placeholder} {...input} />
           ))}
           <div className={`${styles.flexEnd} gap-3`}>
-            <button className={`${styles.buttonLightGray} flex-grow-[1] sm:flex-grow-0`}>Save</button>
-            <button className={`${styles.buttonPurpleOutlined} flex-grow-[1] sm:flex-grow-0`}>Save</button>
+            <button className={`${styles.buttonLightGray} flex-grow-[1] sm:flex-grow-0`} onClick={() => navigate("/")}>
+              Back to Home
+            </button>
+            <button className={`${styles.buttonPurpleOutlined} flex-grow-[1] sm:flex-grow-0`} disabled={isLoading}>
+              {isLoading ? "Loading..." : "Save"}
+            </button>
           </div>
+          {err && <p className="text-[15px] md:text-[16px] mb-1 font-semibold text-red-600">{err}.</p>}
         </div>
       </form>
     </div>
