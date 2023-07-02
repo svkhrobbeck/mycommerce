f0ef29135c35087cbe2a9682adfde0646a44e693
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-02 05:07:38 +0500

    Optimize

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 // style
 import "./index.css";
 
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Provider } from "react-redux";
-import store from "./store/index.ts";
+import { BrowserRouter } from "react-router-dom";
+import ContextProvider from "./context/Context";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
   <ContextProvider>
     <BrowserRouter>
       <App />
