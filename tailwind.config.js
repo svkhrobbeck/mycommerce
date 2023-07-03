e2d5f8122e07a9071ed92efc6c96b2556750f40b
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-03 20:48:47 +0500

    Add simoncat image to tailwind config as bg-image-empty

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: () => ({
+        "bg-image-empty": 'url("images/simoncat-empty.svg")',
+      }),
       colors: {
         dark: "#191919",
       },
