94fd73eedef5358f6a682cc91faad6e0142f81c8
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-03 16:34:22 +0500

    Update product page, fix add to cart functional

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -3,18 +3,25 @@ import { useNavigate, useParams } from "react-router-dom";
 import ProductsService from "../service/products";
 import { IParams, IProduct } from "../interfaces";
 import { styles } from "../constants/styles";
-import { ProductsList, SwiperImageSliders } from "../components";
+import { Loader, ProductsList, SwiperImageSliders } from "../components";
 import { CART_LOCALSTORAGE } from "../constants/constants";
 import { getStorageParse } from "../helpers/localStorage";
-import setToCart from "../helpers/setToCart";
+import toggleCart from "../helpers/toggleCart";
 
 const Product: FC = (): JSX.Element => {
   const navigate = useNavigate();
-  const [keys, setKeys] = useState<number[]>(getStorageParse(CART_LOCALSTORAGE));
+  const [keys, setKeys] = useState<number[]>(getStorageParse(CART_LOCALSTORAGE).map(item => Number(item.id)));
+  const [carts, setCarts] = useState<IProduct[]>(getStorageParse(CART_LOCALSTORAGE));
   const { id } = useParams();
   const [product, setProduct] = useState<IProduct | null>(null);
   const [products, setProducts] = useState<IProduct[]>([]);
   const [params, setParams] = useState<IParams>({});
+
+  const addToCart = (): void => setCarts(toggleCart({ ...product }));
+
+  useEffect((): void => {
+    setKeys(carts.map(cart => Number(cart.id)));
+  }, [carts]);
 
   const getProduct = async (id: string) => {
     const data: IProduct = await ProductsService.getProduct(id);
@@ -37,29 +44,33 @@ const Product: FC = (): JSX.Element => {
     getProducts();
   }, [product]);
 
-  const addToCart = () => setKeys(setToCart(Number(id)));
-
   return (
-    <div className={`${styles.container} py-4 md:py-6`}>
-      <div className="mb-6">
-        {!!product?.id && (
-          <>
-            <h3 className="text-[36px] mb-[6px] font-bold uppercase text-gray-700">{product?.title}</h3>
-            <p className="mb-1.5 max-w-[390px] text-md text-gray-500">{product?.description}</p>
-            <p className="max-w-[390px] mb-2 font-semibold text-xl text-gray-800">Price: ${product?.price}</p>
-            <SwiperImageSliders images={product.images} isOne={false} />
-            <button className={`${styles.buttonYellow}`} onClick={addToCart}>
-              {keys.includes(Number(id)) ? "Added" : "Add to Cart"}
-            </button>
-            <button className={`${styles.buttonGreen} w-full`} onClick={() => navigate("/")}>
-              Back to Home
-            </button>
-          </>
-        )}
-      </div>
-      {!!products.length && <h3 className="text-2xl md:text-4xl lg:text-5xl uppercase mb-3 font-semibold text-center">Similar products</h3>}
-      <ProductsList products={products} />
-    </div>
+    <>
+      {!!product ? (
+        <div className={`${styles.container} py-4 md:py-6`}>
+          <div className="mb-6">
+            <>
+              <h3 className="text-[36px] mb-[6px] font-bold uppercase text-gray-700">{product?.title}</h3>
+              <p className="mb-1.5 max-w-[390px] text-md text-gray-500">{product?.description}</p>
+              <p className="max-w-[390px] mb-2 font-semibold text-xl text-gray-800">Price: ${product?.price}</p>
+              <SwiperImageSliders images={product?.images || []} isOne={false} />
+              <button className={`${styles.buttonYellow} mb-3`} onClick={addToCart}>
+                {keys.includes(Number(id)) ? "Added" : "Add to Cart"}
+              </button>
+              <button className={`${styles.buttonGreen} w-full`} onClick={() => navigate("/")}>
+                Back to Home
+              </button>
+            </>
+          </div>
+          {!!products.length && (
+            <h3 className="text-2xl md:text-4xl lg:text-5xl uppercase mb-3 font-semibold text-center">Similar products</h3>
+          )}
+          <ProductsList products={products} />
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 
