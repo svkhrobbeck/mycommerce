8ce31573247125234eef54aa756490e86d4d1579
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-03 16:36:36 +0500

    Update localStorage helper

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -1,8 +1,10 @@
+import { IProduct } from "../interfaces";
+
 export const setStorage = (key: string, value: string): void => localStorage.setItem(key, value);
 
 export const getStorage = (key: string): string | null => localStorage.getItem(key);
 
-export const getStorageParse = (key: string): number[] => JSON.parse(localStorage.getItem(key) || "[]");
+export const getStorageParse = (key: string): IProduct[] | [] => JSON.parse(localStorage.getItem(key) || "[]");
 
 export const setStorageStringify = (key: string, value: string | object | object[]): void =>
   localStorage.setItem(key, JSON.stringify(value));
