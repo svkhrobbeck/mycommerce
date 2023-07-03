7a802aee9043a7cfe7f63665f60ff1f179519cd4
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-03 21:20:25 +0500

    Update and make responsive loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,10 @@
 import { FC } from "react";
 import { loader } from "../assets";
-import { styles } from "../constants/styles";
 
 const Loader: FC = (): JSX.Element => {
   return (
-    <div className={`${styles.flexCenter} z-[200] fixed top-0 left-0 inset-0`}>
-      <img className="w-[200px] h-auto -mt-[200px]" src={loader} alt="loader" />
+    <div className={`flex justify-center items-start z-[200] fixed top-0 left-0 inset-0`}>
+      <img className="max-w-[90px] mt-[130px] md:max-w-[150px] w-full h-auto" src={loader} alt="loader" />
     </div>
   );
 };
