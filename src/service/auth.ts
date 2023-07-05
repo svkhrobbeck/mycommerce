ae217f3728ff383dfd077bff0c3548ab97de9faf
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-05 09:52:13 +0500

    Optimize

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -6,18 +6,22 @@ const AuthService = {
     const { data } = await axios.post("users", { ...user });
     return data;
   },
+
   async userLogin(user: IAuthUser) {
     const { data } = await axios.post("auth/login", { ...user });
     return data;
   },
+
   async userAvailable(email: string) {
     const { data } = await axios.post("users/is-available", { email });
     return data;
   },
+
   async userGet() {
     const { data } = await axios.get("auth/profile");
     return data;
   },
+
   async userUpdate(id: number, user: IAuthUser) {
     const { data } = await axios.put(`users/${id}`, { ...user });
     return data;
--- src/service/products.ts
+++ src/service/products.ts
@@ -6,10 +6,12 @@ const ProductsService = {
     const { data } = await axios.get("products", { params });
     return data;
   },
+
   async getProduct(id: string) {
     const { data } = await axios.get(`products/${id}`);
     return data;
   },
+
   async getProductCategories() {
     const { data } = await axios.get("categories");
     return data;
