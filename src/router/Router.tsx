ee7f3d29e04d306e7d5cfd3beb44f427e5bf1d97
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-08 15:47:04 +0500

    Update router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -10,22 +10,36 @@ const Router: FC = (): JSX.Element => {
 
   return (
     <Routes>
-      <Route path="/" element={!!auth?.token ? <Home /> : <Navigate to="/login" />} />
-      {!!!auth?.token ? (
-        <Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
-      ) : (
-        <>
-          <Route path="/user" element={<User />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/product/:id" element={<Product />} />
-        </>
-      )}
+      {/* <Route path="/" element={!!auth?.token ? <Home /> : <Navigate to="/login" />} /> */}
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={!!auth?.token ? <Navigate to="/" /> : <Login />} />
+      <Route path="/register" element={!!auth?.token ? <Navigate to="/" /> : <Register />} />
+      <Route path="/user" element={!!auth?.token ? <User /> : <Navigate to="/login" />} />
+      <Route path="/cart" element={!!auth?.token ? <Cart /> : <Navigate to="/login" />} />
+      <Route path="/product/:id" element={<Product />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
+
+  // old routes
+  // return (
+  //   <Routes>
+  //     <Route path="/" element={!!auth?.token ? <Home /> : <Navigate to="/login" />} />
+  //     {!!!auth?.token ? (
+  //       <Route>
+  //         <Route path="/login" element={<Login />} />
+  //         <Route path="/register" element={<Register />} />
+  //       </Route>
+  //     ) : (
+  //       <>
+  //         <Route path="/user" element={<User />} />
+  //         <Route path="/cart" element={<Cart />} />
+  //         <Route path="/product/:id" element={<Product />} />
+  //       </>
+  //     )}
+  //     <Route path="*" element={<PageNotFound />} />
+  //   </Routes>
+  // );
 };
 
 export default Router;
